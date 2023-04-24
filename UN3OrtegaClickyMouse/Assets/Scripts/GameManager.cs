60ec26c776f093b3c884197e6aa04efd247c8e32
NawtiGranni <Nickyy2006@outlook.com>
2023-04-24 09:55:11 -0700

    i made huge progress
    i made the the easy hard mode thing and i also made the game over thing and aswell as the game cant start untl i press any of the diffs

--- UN3OrtegaClickyMouse/Assets/Scripts/GameManager.cs
+++ UN3OrtegaClickyMouse/Assets/Scripts/GameManager.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public List<GameObject> targets;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
     private float spawnRate = 1.0f;
     private int score;
 
@@ -15,9 +20,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+               
     }
 
     // Update is called once per frame
@@ -29,7 +32,7 @@ public class GameManager : MonoBehaviour
    
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -40,5 +43,26 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame()
+    {
+        isGameActive = true;
+        score = 0;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 }
--- UN3OrtegaClickyMouse/Assets/Scripts/Target.cs
+++ UN3OrtegaClickyMouse/Assets/Scripts/Target.cs
@@ -10,9 +10,10 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -1;
 
-
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
 
 
@@ -37,13 +38,22 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }       
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }      
     }
 
     Vector3 RandomForce()
